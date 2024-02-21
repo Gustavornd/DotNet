22c0203fb079e7d5413cec6a50f71c1b59d0c36a
malodex <marcosd3333@gmail.com>
2024-02-21 10:32:55 -0300

    exercicio1

---
+++ Exercicio 1/Program.cs
@@ -0,0 +1,49 @@
+﻿
+//Questão 1:
+        //Exemplo 1;
+
+        int num1 = 5;
+        int num2 = 5;
+        bool result = Equals(num1, num2);
+        Console.WriteLine("Resultado: " + result);
+
+        //Exemplo 2:
+        String str1 = "Gustavo";
+        String str2 = "Gustavo";
+        result = str1.Equals(str2);
+        Console.WriteLine("Resultado: " + result);
+
+        //Exemplo 3:
+        int num3 = 4;
+        int [] numbers = {1,2,3,4,5,6};
+        result = numbers[3].Equals(num3);
+        Console.WriteLine("Resultado: " + result);
+
+
+
+//Questão 2:
+
+        int [] a = {1,2,3,4,5};
+        int [] b = new int[a.Length];
+        Array.Copy(a, b, a.Length);
+        b[0] = 25;
+        b[1] = 30;
+        Console.WriteLine("a: " + a[0] + ", "+a[1]);
+        Console.WriteLine("b: " + b[0] + ", "+b[1]);
+
+
+
+//Questão 3:
+        //Declara a nova lista chamada "nomes" que armazena strings, sendo uma lista vazia de inicio;
+        List<string> nomes = new List<string>(); 
+
+        //O método AddRange adiciona elementos ao final da lista "nomes".
+        nomes.AddRange(new [] {".net", "2023", "ifnmg"});
+
+        //Imprime na tela o tipo do objeto que representa a lista "nomes".
+        Console.WriteLine(nomes);
+
+
+//Questão 4:
+        Console.WriteLine(string.Join(", ", nomes));
+
