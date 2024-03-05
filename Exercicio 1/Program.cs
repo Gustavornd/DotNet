916d89d4677a04f6e3bbff8f9ef56a37160a97b0
malodex <marcosd3333@gmail.com>
2024-03-05 11:02:14 -0300

    dia 05-03

--- Exercicio 1/Program.cs
+++ Exercicio 1/Program.cs
@@ -1,62 +1,85 @@
 ﻿
-//Questão 1:
-        //Exemplo 1;
 
-        int num1 = 5;
-        int num2 = 5;
-        bool result = Equals(num1, num2);
-        Console.WriteLine("Resultado: " + result);
+        //Questão 1:
+                //Exemplo 1;
 
-        //Exemplo 2:
-        String str1 = "Gustavo";
-        String str2 = "Gustavo";
-        result = str1.Equals(str2);
-        Console.WriteLine("Resultado: " + result);
+                int num1 = 5;
+                int num2 = 5;
+                bool result = Equals(num1, num2);
+                Console.WriteLine("Resultado: " + result);
 
-        //Exemplo 3:
-        int num3 = 4;
-        int [] numbers = {1,2,3,4,5,6};
-        result = numbers[3].Equals(num3);
-        Console.WriteLine("Resultado: " + result);
-        /* Saida:
-                Resultado: True
-                Resultado: True
-                Resultado: True
-        */
+                //Exemplo 2:
+                String str1 = "Gustavo";
+                String str2 = "Gustavo";
+                result = str1.Equals(str2);
+                Console.WriteLine("Resultado: " + result);
 
-
-//Questão 2:
-
-        int [] a = {1,2,3,4,5};
-        int [] b = new int[a.Length];
-        Array.Copy(a, b, a.Length);
-        b[0] = 25;
-        b[1] = 30;
-        Console.WriteLine("a: " + a[0] + ", "+a[1]);
-        Console.WriteLine("b: " + b[0] + ", "+b[1]);
-        /* Saida:
-                a: 1, 2
-                b: 25, 30
-        */
+                //Exemplo 3:
+                int num3 = 4;
+                int [] numbers = {1,2,3,4,5,6};
+                result = numbers[3].Equals(num3);
+                Console.WriteLine("Resultado: " + result);
+                /* Saida:
+                        Resultado: True
+                        Resultado: True
+                        Resultado: True 
+                */
 
 
-//Questão 3:
-        //Declara a nova lista chamada "nomes" que armazena strings, sendo uma lista vazia de inicio;
-        List<string> nomes = new List<string>(); 
 
-        //O método AddRange adiciona elementos ao final da lista "nomes".
-        nomes.AddRange(new [] {".net", "2023", "ifnmg"});
+        //Questão 2:
 
-        //Imprime na tela o tipo do objeto que representa a lista "nomes".
-        Console.WriteLine(nomes);
-        /* Saida:
-                System.Collections.Generic.List`1[System.String]
-        */
+                int [] a = {1,2,3,4,5};
+                int [] b = new int[a.Length];
+                Array.Copy(a, b, a.Length);
+                b[0] = 25;
+                b[1] = 30;
+                Console.WriteLine("a: " + a[0] + ", "+a[1]);
+                Console.WriteLine("b: " + b[0] + ", "+b[1]);
+                /* Saida:
+                        a: 1, 2
+                        b: 25, 30
+                        */
 
 
-//Questão 4:
-        Console.WriteLine(string.Join(", ", nomes));
-        /* Saida:
-                .net, 2023, ifnmg
-        */
+
+        //Questão 3:
+                //Declara a nova lista chamada "nomes" que armazena strings, sendo uma lista vazia de inicio;
+                List<string> nomes = new List<string>(); 
+
+                //O método AddRange adiciona elementos ao final da lista "nomes".
+                nomes.AddRange(new [] {".net", "2023", "ifnmg"});
+
+                //Imprime na tela o tipo do objeto que representa a lista "nomes".
+                Console.WriteLine(nomes);
+                /* Saida:
+                        System.Collections.Generic.List`1[System.String]
+                        */
+
+
+
+        //Questão 4:
+
+                Console.WriteLine(string.Join(", ", nomes));
+                /* Saida:
+                        .net, 2023, ifnmg
+                        */
+
+                        
+        //Questão 5:
+
+                int[] matrizA = new int[] { 1, 2, 3 };
+                int[] matrizB = matrizA;
+                matrizA[1] = 20; 
+
+                Console.WriteLine(matrizA.Equals(matrizB));
+
+                int[,] matrizC = new int[,] { { 1, 2 }, { 3, 4 } };
+                int[,] matrizD = matrizC;
+                matrizC[0,1] = 20;
+
+                Console.WriteLine(matrizC.Equals(matrizD));
+                Console.WriteLine(matrizA[1].Equals(matrizB[1]));
+                Console.WriteLine(matrizC[0,1].Equals(matrizD[0,1]));
+
 
---
+++ Exercicio 3/Program.cs
@@ -0,0 +1,51 @@
+﻿int a  = int.Parse(Console.ReadLine() ?? "0");
+int b = int.Parse(Console.ReadLine() ?? "0");
+
+Func<int, int, int> soma = (x,y) => (x+y);
+Func<int, int, int> subtracao = (x,y) => (x-y);
+Func<int, int, int> divisao = (x,y) => (y.Equals(0) ? 0 : (x/y));
+Func<int, int, int> multiplicacao = (x,y) => (x*y);
+Func<int, int, int> resto = (x,y) => (y.Equals(0) ? 0 : (x%y));
+
+
+Console.WriteLine("Soma: " + soma(a,b));
+Console.WriteLine("Subtração: " + subtracao(a,b));
+Console.WriteLine("Divisão: " + divisao(a,b) + " Resto: " + resto(a,b));
+Console.WriteLine("Multiplicação: " + multiplicacao(a,b));
+
+Func<int, int, List<int>> calculadora = (x,y) => {
+        List<int> solucoes = new List<int>();
+        solucoes.Add(x+y);
+        solucoes.Add(x-y);
+        solucoes.Add(y.Equals(0) ? 0 : (x/y));
+        solucoes.Add(x*y);
+        solucoes.Add(y.Equals(0) ? 0 : (x%y));
+        return solucoes;
+};
+
+List<int> resultados = calculadora(a,b);
+
+Console.WriteLine("Resultados no Vetor retornado: ");
+foreach (int item in resultados)
+{       
+        Console.WriteLine(item);
+}
+
+
+/* Saida do Terminal
+
+30
+6
+Soma: 36
+Subtração: 24
+Divisão: 5 Resto: 0
+Multiplicação: 180
+Resultados no Vetor retornado: 
+36
+24
+5
+180
+0
+
+*/
+
