66109b2aedcff21d41039c790c1c8ac871e2000d
malodex <marcosd3333@gmail.com>
2024-03-26 10:33:11 -0300

    26-03

--- Exercicio 7/Program.cs
+++ Exercicio 7/Program.cs
@@ -2,8 +2,14 @@
 
 public class Calculator
 {
-    public static string Calcular(int valor1, int valor2, string operador)
+    public static string Calcular(int? valor1, int? valor2, string? operador)
     {
+        if (valor1 == null)
+            throw new ArgumentNullException(nameof(valor1), "O valor não pode ser nulo.");
+        
+        if (valor2 == null)
+            throw new ArgumentNullExecpion(nameof(valor2), "O valor não pode ser nulo.")
+
         if (operador == null)
             throw new ArgumentNullException(nameof(operador), "O operador não pode ser nulo.");
         
@@ -30,15 +36,51 @@ class Program
 {
     static void Main(string[] args)
     {
-        try
-        {
-            Console.WriteLine(Calculator.Calcular(16, 51, "+"));
-            Console.WriteLine(Calculator.Calcular(16, 51, "*"));
-            Console.WriteLine(Calculator.Calcular(16, 0, "/"));
+        try{
+            Console.WriteLine(Calculator.Calcular(null, 0,"+"));
+        }
+        catch (Exception ex){
+            Console.WriteLine($"Erro: {ex.Message}");
+        }
+
+        
+
+        try{
+            Console.WriteLine(Calculator.Calcular(7, 45, null));
+        }
+        catch (Exception ex){
+            Console.WriteLine($"Erro: {ex.Message}");
+        }
+
+
+        try{
+            Console.WriteLine(Calculator.Calcular(7, 45, "+"));
+        }
+        catch (Exception ex){
+            Console.WriteLine($"Erro: {ex.Message}");
+        }
+
+
+        try{
+            Console.WriteLine(Calculator.Calcular(9, 8, "*"));
+        }
+        catch (Exception ex){
+            Console.WriteLine($"Erro: {ex.Message}");
+        }
+
+
+        try{
+            Console.WriteLine(Calculator.Calcular(16, 0, "/")); // Gera uma exeção de divisão por 0
+        }
+        catch (Exception ex){
+            Console.WriteLine($"Erro: {ex.Message}");
+        }
+
+
+        try{
             Console.WriteLine(Calculator.Calcular(16, 51, "-")); // Gera uma exceção de argumento fora do intervalo
         }
-        catch (Exception ex)
-        {
+        catch (Exception ex){
             Console.WriteLine($"Erro: {ex.Message}");
         }
     }
