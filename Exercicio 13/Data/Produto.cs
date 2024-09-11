27eb9ed78a2b621299046321356888c42ae9d23c
Gustavo Rafael Nunes Durães <gustavo.ipatinga@gmail.com>
2024-09-11 11:05:15 -0300

    exercicio 15 11-09

---
+++ Exercicio 13/Data/Produto.cs
@@ -0,0 +1,9 @@
+namespace Exercicio_13.Data;
+
+public class Produto{
+    public int Id {get; set;}
+    public string? Nome {get; set;}
+    public double Preço {get; set;}
+    public string? Descr {get; set;}
+    public int Quant {get; set;}
+}
